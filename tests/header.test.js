98af76d4b10557516f6ee34ec3e2c7599ab21220
dnp <doanhpham1995@gmail.com>
2018-04-04 22:05:31 -0400

    added an automated test case

---
+++ tests/header.test.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+  // set headless to false if you want to launch browser
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('The header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
