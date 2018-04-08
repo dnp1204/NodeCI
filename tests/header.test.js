1efc88c2bb35401a792522431c94d485664f104f
dnp <doanhpham1995@gmail.com>
2018-04-08 17:48:45 -0400

    resuable functions on page

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,27 +1,32 @@
-const puppeteer = require('puppeteer');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  // set headless to false if you want to launch browser
-  browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('The header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
 
-test('clicking login starts oauth flow', async () => {
+test('Clicking login starts oauth flow', async () => {
   await page.click('.right a');
 
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.getContentOf('a[href="/auth/logout"]');
+  expect(text).toEqual('Logout');
 });
