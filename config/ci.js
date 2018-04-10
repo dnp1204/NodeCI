5e8f6dec5c362d93246e37d3e19e0c486798ce03
dnp <doanhpham1995@gmail.com>
2018-04-10 18:54:34 -0400

    added travis configuration

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://user:password@ds041924.mlab.com:41924/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: 'sajdfl;sadjflksjadf;lsadjflsajdfl;sd;lfj',
   redisUrl: 'redis://127.0.0.1:6379'
 };
