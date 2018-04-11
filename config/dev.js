339873123b65f3a3a5a720c3c7002e787c9e4efe
dnp <doanhpham1995@gmail.com>
2018-04-10 22:25:55 -0400

    added upload routes

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://user:password@ds041924.mlab.com:41924/blog_dev',
   cookieKey: 'sajdfl;sadjflksjadf;lsadjflsajdfl;sd;lfj',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJY3HFDLNX2EHGROQ',
+  secretAccessKey: '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'
 };
