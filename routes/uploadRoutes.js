363a54654ca18b0d999046a7ccf5499976b921d2
dnp <doanhpham1995@gmail.com>
2018-04-11 20:24:19 -0400

    setup upload routes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,7 +3,7 @@ const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 const uuid = require('uuid/v1');
 
-const s3 = AWS.S3({
+const s3 = new AWS.S3({
   accessKeyId: keys.accessKeyId,
   secretAccessKey: keys.secretAccessKey
 });
@@ -12,10 +12,12 @@ module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
     const key = `${req.user.id}/${uuid()}.jpeg`;
 
+    // putObject is the name of the operation we want to create the presigned URL for
+    // check the AWS sdk for javascript documents for more details
     s3.getSignedUrl(
       'putObject',
       {
-        Bucket: 'my-blog-bucket-123',
+        Bucket: 'dnp-blog-bucket-1204',
         ContentType: 'image/jpeg',
         Key: key
       },
