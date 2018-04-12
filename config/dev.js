64c377e0b54233031dd9ba7ebb859a84363974e4
Doanh Pham <doanhpham1995@gmail.com>
2018-04-11 20:32:39 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://user:password@ds041924.mlab.com:41924/blog_dev',
-  cookieKey: 'sajdfl;sadjflksjadf;lsadjflsajdfl;sd;lfj',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
